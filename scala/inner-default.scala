bdcd7ad08ea543cdd57424e851d2fb52b1bc00a0
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2019-12-10 23:13:36 +0100

    Inner trait default argument

---
+++ scala/inner-default.scala
@@ -0,0 +1,14 @@
+class Z{
+	trait T
+	class C extends T
+	def m(x: T = new C) = 1
+}
+object O{
+	def main(args: Array[String]): Unit = {
+		val a = new Z
+		a.m() // OK
+		(new Z).m() // Type mismatch
+	}
+}
+
+// Expect to compile (does in scala 2)
