74631eb4e71b5a204732bfe52ea601360f952d32
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2020-01-15 18:03:18 +0100

    Add comment

--- scala/byname-merge2.scala
+++ scala/byname-merge2.scala
@@ -3,3 +3,4 @@ trait B;
 object O{
    def m(a: (=>A) => Int, b: B => Int, c: Boolean) = if(c) a else b
 }
+// In TypeComparer.andTypeGen, distributeAnd handles the case when both types are ExprType
