a79ad05c3b58614cc8dcb3d84c19e306fce751f3
Vlastimil Dort <dort@d3s.mff.cuni.cz>
2020-01-21 19:41:26 +0100

    Simplify varargs-union

--- scala/varargs-union.scala
+++ scala/varargs-union.scala
@@ -1,8 +1,4 @@
 object O{
-  def m[T](x: T*) = 0
-  def n(b: Boolean): Unit = {
-    val li = List(1)
-    val ls = List("A")
-    m(( if(b) (li: _*) else (ls: _*) ): _*)
-  }
+  def m(x: Any*) = 0
+  def n(a: List[Int] | List[String]) = m(a: _*)
 }
